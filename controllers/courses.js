6ee6a3a871f61cf5aebb15a744836ee51a1a2920
Fandy Barestu <fandy@softwareseni.com>
2019-10-23 15:00:58 +0700

    Create courses ownership validation

--- controllers/courses.js
+++ controllers/courses.js
@@ -51,12 +51,23 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
  */
 exports.addCourse = asyncHandler(async (req, res, next) => {
   req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
 
   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp ${req.params.bootcampId} not found`, 404)
+    );
+  }
+
+  // Make sure user is bootcamp owner or admin
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to add a course to this bootcamp`,
+        401
+      )
     );
   }
 
@@ -80,6 +91,16 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse(`Course ${req.params.id} not found`, 404));
   }
 
+  // Make sure user is bootcamp owner or admin
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to update this course`,
+        401
+      )
+    );
+  }
+
   course = await Course.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true
@@ -101,6 +122,16 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
 
   if (!course) {
     return next(new ErrorResponse(`Course ${req.params.id} not found`, 404));
+  }
+
+  // Make sure user is bootcamp owner or admin
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to delete this course`,
+        401
+      )
+    );
   }
 
   await course.remove();
